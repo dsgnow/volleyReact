ea3d67e28cc15db1d747f538b54aacd8869c0ca3
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2021-03-25 00:39:46 +0100

    date time picker working

--- src/pages/AddGame/AddGame.js
+++ src/pages/AddGame/AddGame.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-wrap-multilines */
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Grid from '@material-ui/core/Grid'
@@ -15,15 +14,13 @@ import {
   StyledTitleTypography
 } from '../../Assets/Styles/GlobalStyles'
 import DateFnsUtils from '@date-io/date-fns' // choose your lib
-import {
-  DatePicker,
-  TimePicker,
-  DateTimePicker,
-  MuiPickersUtilsProvider
-} from '@material-ui/pickers'
+import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
+import { format, addHours } from 'date-fns'
 import plLocale from 'date-fns/locale/pl'
+import DateRangeOutlinedIcon from '@material-ui/icons/DateRangeOutlined'
+import { IconButton, InputAdornment } from '@material-ui/core'
 
-import { useFormik, Field } from 'formik'
+import { useFormik, Field, Form } from 'formik'
 import * as yup from 'yup'
 
 const StyledContainer = styled(Container)`
@@ -58,29 +55,48 @@ const StyledTypography = styled(Typography)`
     text-align: left;
   }
 `
-let today = new Date()
-const dd = String(today.getDate()).padStart(2, '0')
-const mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
-const yyyy = today.getFullYear()
-
-today = yyyy + '-' + mm + '-' + dd
+class LocalizedUtils extends DateFnsUtils {
+  getDatePickerHeaderText(date) {
+    return format(date, 'd MMM yyyy', { locale: this.locale })
+  }
+}
 
 let validationSchema = yup.object().shape({
   name: yup.string().required('To pole jest wymagane..'),
   city: yup.string().required('To pole jest wymagane..'),
   street: yup.string().required('To pole jest wymagane..'),
-  timeStart: yup.string().required('To pole jest wymagane..'),
-  timeEnd: yup.string().required('To pole jest wymagane..'),
+  dateStart: yup.date().required('To pole jest wymagane..'),
+  dateEnd: yup
+    .date()
+    .min(
+      yup.ref('dateStart'),
+      'czas zakończenia musi być późniejszy niż rozpoczęcia'
+    )
+    .required('To pole jest wymagane..'),
   places: yup.number().required('To pole jest wymagane..'),
   level: yup.string().required('To pole jest wymagane..'),
   price: yup.number().required('To pole jest wymagane..'),
-  // rotationTime1: yup.string().required('To pole jest wymagane..'),
-  rotationTime2: yup.string().required('To pole jest wymagane..'),
-  rotationTime3: yup.string().required('To pole jest wymagane..'),
-  rotationTime1: yup.string().when('rotationTime2', {
-    is: '00:00',
-    then: yup.string().required('Nie może być 00')
-  })
+  rotationTime1: yup
+    .date()
+    .min(
+      yup.ref('dateStart'),
+      'godzina tej rotacji musi być późniejsza niż rozpoczęcie gry'
+    )
+    .required('To pole jest wymagane..'),
+  rotationTime2: yup
+    .date()
+    .min(
+      yup.ref('rotationTime1'),
+      'godzina tej rotacji musi być późniejsza niż poprzedniej'
+    )
+    .required('To pole jest wymagane..'),
+  rotationTime3: yup
+    .date()
+    .min(
+      yup.ref('rotationTime2'),
+      'godzina tej rotacji musi być późniejsza niż poprzedniej'
+    )
+    .required('To pole jest wymagane..')
 })
 
 const AddGame = () => {
@@ -89,23 +105,23 @@ const AddGame = () => {
       name: '',
       city: '',
       street: '',
-      date: new Date(),
-      timeStart: '21:30',
-      timeEnd: '03:00',
+      dateStart: new Date(),
+      dateEnd: addHours(new Date(), 4),
       places: '',
       level: '',
       price: '',
       autoSquads: false,
-      rotationTime1: '00:00',
-      rotationTime2: '01:00',
-      rotationTime3: '02:00'
+      rotationTime1: addHours(new Date(), 1),
+      rotationTime2: addHours(new Date(), 2),
+      rotationTime3: addHours(new Date(), 3)
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2))
     }
   })
-  console.log(formik.initialValues)
+
+  const { touched, handleChange, setFieldValue, errors, values } = formik
 
   return (
     <StyledContainer>
@@ -118,14 +134,14 @@ const AddGame = () => {
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <TextField
-                error={formik.touched.name && Boolean(formik.errors.name)}
-                helperText={formik.touched.name && formik.errors.name}
+                error={touched.name && Boolean(errors.name)}
+                helperText={touched.name && errors.name}
                 autoComplete="name"
                 name="name"
                 variant="outlined"
                 fullWidth
-                value={formik.values.name}
-                onChange={formik.handleChange}
+                value={values.name}
+                onChange={handleChange}
                 id="name"
                 label="Nazwa hali"
                 size="small"
@@ -133,12 +149,12 @@ const AddGame = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
-                error={formik.touched.city && Boolean(formik.errors.city)}
-                helperText={formik.touched.city && formik.errors.city}
+                error={touched.city && Boolean(errors.city)}
+                helperText={touched.city && errors.city}
                 variant="outlined"
                 fullWidth
-                value={formik.values.city}
-                onChange={formik.handleChange}
+                value={values.city}
+                onChange={handleChange}
                 id="city"
                 label="Miasto"
                 size="small"
@@ -148,12 +164,12 @@ const AddGame = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
-                error={formik.touched.street && Boolean(formik.errors.street)}
-                helperText={formik.touched.street && formik.errors.street}
+                error={touched.street && Boolean(errors.street)}
+                helperText={touched.street && errors.street}
                 variant="outlined"
                 fullWidth
-                value={formik.values.street}
-                onChange={formik.handleChange}
+                value={values.street}
+                onChange={handleChange}
                 id="street"
                 label="Ulica"
                 size="small"
@@ -162,71 +178,79 @@ const AddGame = () => {
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              {/* <TextField
-                error={formik.touched.date && Boolean(formik.errors.date)}
-                helperText={formik.touched.date && formik.errors.date}
-                variant="outlined"
-                fullWidth
-                value={formik.values.date}
-                onChange={formik.handleChange}
-                id="date"
-                size="small"
-                type="date"
-                name="date"
-                label="Data"
-              /> */}
-              <MuiPickersUtilsProvider locale={plLocale} utils={DateFnsUtils}>
+              <MuiPickersUtilsProvider locale={plLocale} utils={LocalizedUtils}>
                 <DateTimePicker
-                  label="DateTimePicker"
+                  error={touched.dateStart && Boolean(errors.dateStart)}
+                  helperText={touched.dateStart && errors.dateStart}
+                  size="small"
+                  fullWidth
+                  label="Data rozpoczęcia"
                   inputVariant="outlined"
-                  value={formik.values.date}
-                  onChange={formik.handleChange}
+                  name={'dateStart'}
+                  value={values.dateStart}
+                  clearable
+                  format="d MMM yyyy HH:mm"
+                  disablePast={true}
+                  ampm={false}
+                  clearLabel="wyczyść"
+                  cancelLabel="anuluj"
+                  onChange={(e) => setFieldValue('dateStart', e)}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton>
+                          <DateRangeOutlinedIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
                 />
               </MuiPickersUtilsProvider>
             </Grid>
-            <Grid item xs={6} sm={3}>
-              <TextField
-                error={
-                  formik.touched.timeStart && Boolean(formik.errors.timeStart)
-                }
-                helperText={formik.touched.timeStart && formik.errors.timeStart}
-                variant="outlined"
-                fullWidth
-                value={formik.values.timeStart}
-                onChange={formik.handleChange}
-                id="timeStart"
-                size="small"
-                type="time"
-                label="Rozpoczęcie"
-                name="timeStart"
-              />
-            </Grid>
-            <Grid item xs={6} sm={3}>
-              <TextField
-                error={formik.touched.timeEnd && Boolean(formik.errors.timeEnd)}
-                helperText={formik.touched.timeEnd && formik.errors.timeEnd}
-                variant="outlined"
-                fullWidth
-                value={formik.values.timeEnd}
-                onChange={formik.handleChange}
-                id="timeEnd"
-                size="small"
-                type="time"
-                label="Zakończenie"
-                name="timeEnd"
-              />
+            <Grid item xs={12} sm={6}>
+              <MuiPickersUtilsProvider locale={plLocale} utils={LocalizedUtils}>
+                <DateTimePicker
+                  error={touched.dateEnd && Boolean(errors.dateEnd)}
+                  helperText={touched.dateEnd && errors.dateEnd}
+                  minDateTime={values.dateStart}
+                  placeholder={addHours(new Date(), 1)}
+                  size="small"
+                  fullWidth
+                  label="Data zakończenia"
+                  inputVariant="outlined"
+                  name={'dateEnd'}
+                  value={values.dateEnd}
+                  clearable
+                  format="d MMM yyyy HH:mm"
+                  disablePast={true}
+                  ampm={false}
+                  clearLabel="wyczyść"
+                  cancelLabel="anuluj"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton>
+                          <DateRangeOutlinedIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                  onChange={(e) => setFieldValue('dateEnd', e)}
+                />
+              </MuiPickersUtilsProvider>
             </Grid>
             <Grid item xs={12} sm={3}>
               <TextField
-                error={formik.touched.level && Boolean(formik.errors.level)}
-                helperText={formik.touched.level && formik.errors.level}
+                size="small"
+                error={touched.level && Boolean(errors.level)}
+                helperText={touched.level && errors.level}
                 fullWidth
                 id="level"
                 name="level"
                 select
                 label="Poziom"
-                value={formik.values.level}
-                onChange={formik.handleChange}
+                value={values.level}
+                onChange={handleChange}
                 variant="outlined">
                 <MenuItem value="Rekreacyjny">Rekreacyjny</MenuItem>
                 <MenuItem value="Amatorski">Amatorski</MenuItem>
@@ -235,13 +259,13 @@ const AddGame = () => {
             </Grid>
             <Grid item xs={12} sm={3}>
               <TextField
-                error={formik.touched.places && Boolean(formik.errors.places)}
-                helperText={formik.touched.places && formik.errors.places}
+                error={touched.places && Boolean(errors.places)}
+                helperText={touched.places && errors.places}
                 variant="outlined"
                 type="number"
                 fullWidth
-                value={formik.values.places}
-                onChange={formik.handleChange}
+                value={values.places}
+                onChange={handleChange}
                 id="places"
                 label="Ilość miejsc"
                 size="small"
@@ -251,13 +275,13 @@ const AddGame = () => {
             </Grid>
             <Grid item xs={12} sm={3}>
               <TextField
-                error={formik.touched.price && Boolean(formik.errors.price)}
-                helperText={formik.touched.price && formik.errors.price}
+                error={touched.price && Boolean(errors.price)}
+                helperText={touched.price && errors.price}
                 variant="outlined"
                 type="number"
                 fullWidth
-                value={formik.values.price}
-                onChange={formik.handleChange}
+                value={values.price}
+                onChange={handleChange}
                 id="price"
                 label="Cena za osobę"
                 size="small"
@@ -266,12 +290,12 @@ const AddGame = () => {
               />
             </Grid>
           </Grid>
-          <Grid item xs={12} sm={12}>
+          <Grid item xs={12} sm={12} style={{ margin: '20px 0' }}>
             <FormControlLabel
               control={
                 <Switch
-                  autoSquads={formik.values.autoSquads}
-                  onChange={formik.handleChange}
+                  autoSquads={values.autoSquads}
+                  onChange={handleChange}
                   name="autoSquads"
                   color="primary"
                 />
@@ -279,32 +303,54 @@ const AddGame = () => {
               label="Dodaj ponowne wybranie składów"
             />
           </Grid>
-          {formik.values.autoSquads && (
+          {values.autoSquads && (
             <Grid container spacing={2}>
               {[...Array(3)].map((item, index) => {
                 let rotationName = `rotationTime${index + 1}`
-                console.log(rotationName)
+                let beforeRotationName = `rotationTime${index}`
+                console.log(index, rotationName)
                 return (
-                  <Grid key={index} item xs={6} sm={3}>
-                    <TextField
-                      error={
-                        formik.touched[rotationName] &&
-                        Boolean(formik.errors[rotationName])
-                      }
-                      helperText={
-                        formik.touched[rotationName] &&
-                        formik.errors[rotationName]
-                      }
-                      variant="outlined"
-                      fullWidth
-                      value={formik.values[rotationName]}
-                      onChange={formik.handleChange}
-                      id={rotationName}
-                      size="small"
-                      type="time"
-                      label={`Rotacja ${index + 1}`}
-                      name={rotationName}
-                    />
+                  <Grid key={index} item xs={12} sm={6}>
+                    <MuiPickersUtilsProvider
+                      locale={plLocale}
+                      utils={LocalizedUtils}>
+                      <DateTimePicker
+                        error={
+                          touched[rotationName] && Boolean(errors[rotationName])
+                        }
+                        helperText={
+                          touched[rotationName] && errors[rotationName]
+                        }
+                        minDateTime={
+                          index === 0
+                            ? values.dateStart
+                            : values[beforeRotationName]
+                        }
+                        maxDateTime={values.dateEnd}
+                        size="small"
+                        fullWidth
+                        label={`Rotacja${index + 1}`}
+                        inputVariant="outlined"
+                        name={rotationName}
+                        value={values[rotationName]}
+                        clearable
+                        format="d MMM yyyy HH:mm"
+                        disablePast={true}
+                        ampm={false}
+                        clearLabel="wyczyść"
+                        cancelLabel="anuluj"
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <IconButton>
+                                <DateRangeOutlinedIcon />
+                              </IconButton>
+                            </InputAdornment>
+                          )
+                        }}
+                        onChange={(e) => setFieldValue(rotationName, e)}
+                      />
+                    </MuiPickersUtilsProvider>
                   </Grid>
                 )
               })}
