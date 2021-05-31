a43f3252bc9a25d1e63a5d503b95007fa686ddd6
Piotr Stachowicz <ptr.stachowicz@gmail.com>
2021-05-31 13:09:45 +0200

    remove logout when expiry date

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,21 +1,21 @@
-import { useContext, useDebugValue, useEffect } from 'react'
+import { useContext, useDebugValue } from 'react'
 import AuthContext from '../context/authContext'
-import { parseISO, isBefore } from 'date-fns'
-import { useHistory } from 'react-router-dom'
+// import { parseISO, isBefore } from 'date-fns'
+// import { useHistory } from 'react-router-dom'
 
 export default function useAuth() {
   const authContext = useContext(AuthContext)
   const auth = authContext.user
 
-  const history = useHistory()
+  // const history = useHistory()
 
-  useEffect(() => {
-    if (auth && isBefore(parseISO(auth.expiryDate), new Date())) {
-      authContext.logout()
-      window.localStorage.removeItem('token-data')
-      history.push('/logowanie')
-    }
-  })
+  // useEffect(() => {
+  //   if (auth && isBefore(parseISO(auth.expiryDate), new Date())) {
+  //     authContext.logout()
+  //     window.localStorage.removeItem('token-data')
+  //     history.push('/logowanie')
+  //   }
+  // })
 
   useDebugValue(auth ? 'Zalogowany' : 'Wylogowany')
 
